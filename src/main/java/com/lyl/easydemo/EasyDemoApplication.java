67696819f865323d85547ef7debf9ead0a5c27c9
liuyuelei <hsm8oPd6JlxmMiwiJklC>
2022-02-15 11:50:22 +0800

    创建项目

---
+++ src/main/java/com/lyl/easydemo/EasyDemoApplication.java
@@ -0,0 +1,21 @@
+package com.lyl.easydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class EasyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDemoApplication.class, args);
+    }
+
+    @GetMapping
+    public String test(){
+        return "TEST";
+    }
+
+}
